fa8a778471f7f39bea3ec3222ffe39f72cdb8e10
Zwe Lwin Oo <zwelwin.oo@partnerassociates.com>
2026-01-28 15:19:08 +0630

    chore: remove unused room-related validation and app settings

--- src/BookingSystem.Application/Common/Settings/ValidationSettings.cs
+++ src/BookingSystem.Application/Common/Settings/ValidationSettings.cs
@@ -5,7 +5,6 @@ public class ValidationSettings
     public const string SectionName = "ValidationSettings";
 
     public int MaxGuestsPerBooking { get; set; } = 10;
-    public int MaxRoomCapacity { get; set; } = 10;
     public int MaxBookingDaysInAdvance { get; set; } = 365;
     public bool AllowSameDayCheckIn { get; set; } = true;
     
@@ -13,9 +12,6 @@ public class ValidationSettings
     
     public class FieldLengthLimits
     {
-        public int RoomNumber { get; set; } = 50;
-        public int RoomType { get; set; } = 100;
-        public int RoomDescription { get; set; } = 500;
         public int FirstName { get; set; } = 100;
         public int LastName { get; set; } = 100;
         public int Email { get; set; } = 200;
